70dce97baa0f890f7d3aac4ca1e6547eaf432003
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-09-26 20:08:39 -0500

    :heavy_plus_sign: Add Jest with enzyme

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  roots: [
+    '<rootDir>/src',
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/jest.setup.js',
+  ],
+  testRunner: 'jest-circus/runner',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  resetMocks: true,
+};
---
+++ src/__tests__/example.test.jsx
@@ -0,0 +1,6 @@
+describe('Example test', () => {
+  test('should return true', () => {
+    const haveTests = true;
+    expect(haveTests).toBeTruthy();
+  });
+});
