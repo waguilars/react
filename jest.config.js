d161d62dd32e70deee6ac5b076bbb699e519a417
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-10-01 13:02:48 -0500

    :sparkles: Add support for css.module

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  roots: [
-    '<rootDir>/src',
-  ],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/jest.setup.js',
-  ],
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.js'],
   testRunner: 'jest-circus/runner',
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
   ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
