bbe428acf5e4a8a35758d5c93db3182dcd0055bf
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2022-03-02 13:08:33 -0500

    :hammer: chore: update deps

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Hello Vite + React!</p>
         <Counter />
         <p>
@@ -16,19 +16,19 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
